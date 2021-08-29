49fcbc33d1acc0df130635484ad242316fc299f2
ArtemAtoian <89732675+ArtemAtoian@users.noreply.github.com>
2021-08-29 16:03:38 +0300

    Add files via upload

---
+++ extension/popup.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    var str = document.getElementsByClassName("row-2").value
+    var elifstr = "Просмотр видео "
+    var elifelifstr = "Переход + Просм"
+    var counter = 0
+    console.log(document.getElementsByClassName("row-2").value)
+}
